63d799a893bcdfdb6cbbf9ebacbaa3a7a72d6d00
SahilKokate <sahilkokate92@gmail.com>
2026-09-15 23:00:17 +0530

    added mx, my, my in propulsion

--- PropulsionModel.py
+++ PropulsionModel.py
@@ -6,4 +6,8 @@ class PropulsionModel:
            self.Fx_prop = del_t * self.T_max
            self.Fy_prop = 0
            self.Fz_prop = 0
-           return self.Fx_prop, self.Fy_prop, self.Fz_prop
+           self.Mx_prop = 0
+           self.My_prop = 0
+           self.Mz_prop = 0
+           return self.Fx_prop, self.Fy_prop, self.Fz_prop, self.Mx_prop, self.My_prop, self.Mz_prop
+    
