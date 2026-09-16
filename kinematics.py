0af26b98fdf58b81ec42203f57fcc11f5a80d54c
SahilKokate <sahilkokate92@gmail.com>
2026-09-17 00:33:57 +0530

    updated dcm

--- kinematics.py
+++ kinematics.py
@@ -4,7 +4,7 @@ def R(phi,theta,psi):
 
     R = np.array([
         [np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi)],
-        [np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.sin(psi) + np.cos(phi)*np.cos(psi), np.cos(phi)*np.sin(theta)*np.sin(psi) - np.sin(phi)*np.cos(psi)],
+        [np.cos(theta)*np.sin(psi), np.sin(phi)*np.sin(theta)*np.sin(psi) + np.cos(phi)*np.cos(psi), np.cos(phi)*np.sin(theta)*np.sin(psi) - np.sin(phi)*np.cos(psi)],
         [-np.sin(theta), np.sin(phi)*np.cos(theta), np.cos(phi)*np.cos(theta)]
     ])
 
