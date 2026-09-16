94e6c5d8579a44efd84be5380d2510d6195c4511
SahilKokate <sahilkokate92@gmail.com>
2026-09-17 00:40:58 +0530

    modified CL, FX and Fz

--- Aerodyanmic_Model.py
+++ Aerodyanmic_Model.py
@@ -24,14 +24,14 @@ class AerodynamicsModel:
         self.r_hat = (r*self.parameters.b)/(2*Vt)
 
         # Computation of Aerodynamics force coefficients
-        self.CL = (self.parameters.CL0) + (self.parameters.CL_alpha*alpha) + (self.parameters.CL_q*self.p_hat) + (self.parameters.CL_de*del_e)
+        self.CL = (self.parameters.CL0) + (self.parameters.CL_alpha*alpha) + (self.parameters.CL_q*self.q_hat) + (self.parameters.CL_de*del_e)
         self.CD = (self.parameters.CD0) + (self.parameters.CD_alpha*alpha) + (self.parameters.CD_q*self.q_hat) + (self.parameters.CD_de*del_e)
         self.CY = (self.parameters.CY_beta*beta) + (self.parameters.CY_p*self.p_hat) + (self.parameters.CY_r*self.r_hat) + (self.parameters.CY_da*del_a) + (self.parameters.CY_dr*del_r)
 
         # Moment coefficents 
         self.Cl = (self.parameters.Cl_beta*beta) + (self.parameters.Cl_p*self.p_hat) + (self.parameters.Cl_r*self.r_hat) + (self.parameters.Cl_da*del_a) + (self.parameters.Cl_dr*del_r)
         self.Cm = (self.parameters.Cm0) + (self.parameters.Cm_alpha*alpha) + (self.parameters.Cm_q*self.q_hat) + (self.parameters.Cm_de*del_e)
-        self.Cn = (self.parameters.Cn_beta*beta) + (self.parameters.Cn_p*self.p_hat) + (self.parameters.Cn_r*self.r_hat) + (self.parameters.Cn_da*del_a) + (self.parameters.Cn_r*del_r)
+        self.Cn = (self.parameters.Cn_beta*beta) + (self.parameters.Cn_p*self.p_hat) + (self.parameters.Cn_r*self.r_hat) + (self.parameters.Cn_da*del_a) + (self.parameters.Cn_dr*del_r)
 
         # Computing Aerodynamic Forces and Moments
         self.q_dynamic = 0.5 * rho * Vt**2
@@ -45,9 +45,9 @@ class AerodynamicsModel:
 
         # Wind axis to Body axis forces tranformation
 
-        self.Fx_aero = -self.D_aeroF * np.cos(alpha)*np.cos(beta) - self.Y_aerof * np.cos(alpha)*np.sin(beta) + self.L_aeroM * np.sin(alpha)
+        self.Fx_aero = -self.D_aeroF * np.cos(alpha)*np.cos(beta) - self.Y_aerof * np.cos(alpha)*np.sin(beta) + self.L_aeroF * np.sin(alpha)
         self.Fy_aero = -self.D_aeroF * np.sin(beta) + self.Y_aerof * np.cos(beta)
-        self.Fz_aero = -self.D_aeroF * np.sin(alpha)*np.cos(beta) - self.Y_aerof*np.sin(alpha)*np.sin(beta) - self.L_aeroM * np.cos(alpha)
+        self.Fz_aero = -self.D_aeroF * np.sin(alpha)*np.cos(beta) - self.Y_aerof*np.sin(alpha)*np.sin(beta) - self.L_aeroF * np.cos(alpha)
 
         return self.Fx_aero, self.Fy_aero, self.Fz_aero, self.L_aeroM, self.M_aeroM, self.N_aeroM
     
