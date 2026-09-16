49a371a293835ff249c5b652230ab011058e8a3c
SahilKokate <sahilkokate92@gmail.com>
2026-09-16 09:05:30 +0530

    added dcm and euler rate matrix

--- kinematics.py
+++ kinematics.py
@@ -1,6 +1,20 @@
 import numpy as np
-from RigidBodyDynamics import FlightDynamics
 
-class DCM:
-    def __init__(self, dynamics: FlightDynamics):
-        self.dynamics = dynamics
+def R(phi,theta,psi):
+
+    R = np.array([
+        [np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi)],
+        [np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.sin(psi) + np.cos(phi)*np.cos(psi), np.cos(phi)*np.sin(theta)*np.sin(psi) - np.sin(phi)*np.cos(psi)],
+        [-np.sin(theta), np.sin(phi)*np.cos(theta), np.cos(phi)*np.cos(theta)]
+    ])
+
+    return R
+
+def H(phi,theta):
+    H = np.array([
+        [1, np.sin(phi)*np.tan(theta), np.cos(phi)*np.tan(theta)],
+        [0, np.cos(phi), -np.sin(phi)],
+        [0, np.sin(phi)*np.sec(theta), np.cos(phi)*np.sec(theta)]
+    ])
+
+    return H
